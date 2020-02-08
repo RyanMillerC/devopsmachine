fd00c8acf2932f7cf4b1da54205a954928067e5e
Ryan Miller <ryan@devopsmachine.com>
2020-02-08 11:33:37 -0500

    Refactor App to use hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import Header from 'components/header/Header'
-import LinkList from 'components/link/LinkList'
-import Skills from 'components/skills/Skills'
-import Title from 'components/title/Title'
-import ViewSource from 'components/viewSource/ViewSource'
+import Header from 'components/header/Header';
+import LinkList from 'components/link/LinkList';
+import Skills from 'components/skills/Skills';
+import Title from 'components/title/Title';
+import ViewSource from 'components/viewSource/ViewSource';
 
-import links from 'components/link/links'
-import skillsMap from 'components/skills/skillsMap'
+import links from 'components/link/links';
+import skills from 'components/skills/skillsMap';
 
-import './app.css'
+import './app.css';
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
 
-    this.state = {
-      links: links,
-      skills: skillsMap
-    }
-  }
-
-  render () {
-    return (
-      <div className='app'>
-        <div> {/* Container to center with flex */}
-          <Header>Ryan Miller</Header>
-          <Title>
-            Full-Stack Application Developer
-          </Title>
-          <Skills setTitle={this.setTitle} skills={this.state.skills} />
-          <LinkList links={this.state.links} />
-        </div>
-        <ViewSource /> {/* Absolute positioned; exists outside container */}
+const App = () => {
+  return (
+    <div className='app'>
+      <div> {/* Container to center with flex */}
+        <Header>Ryan Miller</Header>
+        <Title>
+          Subject Matter Expert (DevSecOps)
+        </Title>
+        <Skills skills={skills} />
+        <LinkList links={links} />
       </div>
-    )
-  }
+      <ViewSource /> {/* Absolute positioned; exists outside container */}
+    </div>
+  );
 }
 
-export default App
+export default App;
